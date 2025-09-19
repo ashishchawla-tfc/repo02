ae09ace0241d038750b13bdc5e744edaefcdb70b
Ashish Chawla <159796100+ashishchawla-tfc@users.noreply.github.com>
2025-09-19 08:51:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+variable "example_var" {
+  description = "Example variable to trigger a plan change"
+  type        = string
+  default     = "hello"
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    var_value = var.example_var
+  }
+}
